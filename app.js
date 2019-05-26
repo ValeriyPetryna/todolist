96ea72851e32a3457453e2d226ae729cafef8cbd
ValeriyPetryna <valeriypetrina@gmail.com>
2019-05-27 00:18:27 +0300

    add delete-button

--- app.js
+++ app.js
@@ -3,11 +3,34 @@ function newTask() {
   let inputValue = document.getElementById("createTask").value
   let task = document.createTextNode(inputValue)
   li.appendChild(task)
-
-    if (inputValue === '') {
-        alert("Task is clear!")
+ 
+  if (inputValue === '') {
+      alert("Task is clear!")
     } else {
-        document.getElementById("tasks").appendChild(li)
-      }
+      document.getElementById("tasks").appendChild(li)
+  }
+     
   document.getElementById("createTask").value = ""
+  let btn = document.createElement("BUTTON")
+  let deleteSymbol = document.createTextNode("X")
+  btn.className = "close"
+  btn.appendChild(deleteSymbol)
+  li.appendChild(btn)
+  btn.onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+document.body.onkeyup = function(e) {
+  if (e.keyCode == 13) {
+    newTask()
+    
+  }
+}
+let close = document.getElementsByClassName("close")
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement
+    div.style.display = "none"
+  }
 }
