dd28853242c82ab5788ed8657192d51890e75b5c
ValeriyPetryna <valeriypetrina@gmail.com>
2019-05-27 10:58:42 +0300

    add some corrections

--- app.js
+++ app.js
@@ -1,45 +1,59 @@
 function newTask() {
-  let li = document.createElement("li")
-  let inputValue = document.getElementById("createTask").value
-  let task = document.createTextNode(inputValue)
+  const li = document.createElement("li")
+  const inputValue = document.getElementById("createTask").value
+  const task = document.createTextNode(inputValue)
   li.appendChild(task)
- 
+
   if (inputValue === '') {
-      alert("Task is clear!")
+      alert("Task is empty!")
     } else {
       document.getElementById("tasks").appendChild(li)
   }
-     
+ 
   document.getElementById("createTask").value = ""
-  let btn = document.createElement("BUTTON")
-  let deleteSymbol = document.createTextNode("X")
+  const btn = document.createElement("BUTTON")
+  const deleteSymbol = document.createTextNode("X")
   btn.className = "close"
   btn.appendChild(deleteSymbol)
   li.appendChild(btn)
   btn.onclick = function() {
-    let div = this.parentElement;
-    div.style.display = "none";
+    const div = this.parentElement
+    div.style.display = "none"
   }
 }
 document.body.onkeyup = function(e) {
   if (e.keyCode == 13) {
     newTask()
-    
   }
 }
-let close = document.getElementsByClassName("close")
+const close = document.getElementsByClassName("close")
 for (let i = 0; i < close.length; i++) {
   close[i].onclick = function() {
-    let div = this.parentElement
+    const div = this.parentElement
     div.style.display = "none"
   }
 }
-function checked() {
-  let list = document.querySelector('ul')
+function markAsDone() {
+  const list = document.querySelector('ul')
   list.addEventListener('click', function(task) {
     if (task.target.tagName === 'LI') {
-      task.target.classList.toggle('checked')
+      task.target.classList.toggle('markAsDone')
     }
   }, false)
 }
-checked()
+markAsDone()
+
+/* Search-task feature logic can be like this : 
+==> push all task to array,
+function newTask(text) { 
+  allTasks.push(text) 
+}
+==> filter this tasks,
+function searchTask(text) { 
+  displayedTasks = allTasks.filter('search filter') 
+}
+==> and render to page filtered tasks,
+function renderToPage() {
+  filteredTasks.forEach('render to page here')
+}*/
+
